f942c1af5f5b6b00ad762e9f96912a2c63cda003
Gareth Kirwan <gbjk@thermeon.com>
2016-10-12 17:30:53 +0100

    Added ping response

--- relay/http.go
+++ relay/http.go
@@ -112,6 +112,12 @@ func (h *HTTP) Stop() error {
 
 func (h *HTTP) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+
+	if r.URL.Path == "/ping" && (r.Method == "GET" || r.Method == "HEAD") {
+		w.Header().Add("X-InfluxDB-Version", "relay")
+		return
+	}
+
 	if r.URL.Path != "/write" {
 		jsonError(w, http.StatusNotFound, "invalid write endpoint")
 		return
